1d02d72abea711ac4516cd34d634b0bd82e813ac
AlexKeehan <alexk2678@yahoo.com>
2022-11-22 10:03:10 -0500

    Adding Classes

--- Card.java
+++ Card.java
@@ -12,5 +12,20 @@ public class Card {
 
     public int getValue() { return value; }
 
-
+    public String toString() {
+        String printVal;
+        if (value == 11) {
+            printVal = "Jack";
+        } else if (value == 12) {
+            printVal = "Queen";
+        } else if (value == 13) {
+            printVal = "King";
+        } else if (value == 14) {
+            printVal = "Ace";
+        } else {
+            printVal = "" + value;
+        }
+        String result = printVal + " of " + suit;
+        return result;
+    }
 }
--- Table.java
+++ Table.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.Random;
 public class Table {
+    private ArrayList<Card> cards;
+
+    public Table() {
+        cards = new ArrayList<>();
+    }
+
+    public void add52() {
+        for (Suit s : Suit.values()) {
+            for(int i = 2; i <= 14; i++) {
+                cards.add(new Card(s, i));
+            }
+        }
+    }
+
+    public Card deal() {
+        Card top = cards.get(0);
+        cards.remove(0);
+        return top;
+    }
+
+    public void shuffle() {
+        Random rng = new Random();
+        ArrayList<Card> shuffled = new ArrayList<>();
+        while (cards.size() > 0) {
+            int index = rng.nextInt(cards.size());
+            Card c = cards.get(index);
+            cards.remove(index);
+            shuffled.add(c);
+        }
+        cards = shuffled;
+    }
 }
