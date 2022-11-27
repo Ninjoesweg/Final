baf101013953c2ed38b4da72ad73e72b500fd6ba
ByronWilli <102750452+ByronWilli@users.noreply.github.com>
2022-11-26 21:06:15 -0500

    Added Javadocs to Table Class

--- Table.java
+++ Table.java
@@ -8,6 +8,9 @@ public class Table {
         cards = new ArrayList<>();
     }
 
+    /**
+     * This method creates a deck by adding 52 Cards to cards ArrayList
+     */
     public void add52() {
         for (Suit s : Suit.values()) {
             for(int i = 2; i <= 14; i++) {
@@ -16,11 +19,20 @@ public class Table {
         }
     }
 
+    /**
+     * This method is called to deal the top card in the deck
+     * @return Card at top of deck
+     */
     public Card deal() {
         Card top = cards.get(0);
         cards.remove(0);
         return top;
     }
+
+    /**
+     * This method deals the first 3 faceup cards (the flop)
+     * @return an ArrayList of Card objects
+     */
     public ArrayList<Card> flop(){
         for(int i=0; i<3; i++){
         faceUp.add(deal());
@@ -28,6 +40,9 @@ public class Table {
         return(faceUp);
     }
 
+    /**
+     * This method shuffles the Card objects in the cards ArrayList
+     */
     public void shuffle() {
         Random rng = new Random();
         ArrayList<Card> shuffled = new ArrayList<>();
